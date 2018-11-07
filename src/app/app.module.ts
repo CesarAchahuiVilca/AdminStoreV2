bee6eab45a46627f95329498d0059afda61dd261
paultbr <paultheo65@gmail.com>
2018-11-07 10:25:38 -0500

    componentemarca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { MarcadistriComponent } from './marcadistri/marcadistri.component';
+import { RouterModule, Route } from '@angular/router';
+
+const routes: Route[] = [
+  {path: 'marca', component: MarcadistriComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MenuComponent,
-    BreadcrumbComponent
+    BreadcrumbComponent,
+    MarcadistriComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/marcadistri/marcadistri.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marcadistri',
+  templateUrl: './marcadistri.component.html',
+  styleUrls: ['./marcadistri.component.css']
+})
+export class MarcadistriComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
