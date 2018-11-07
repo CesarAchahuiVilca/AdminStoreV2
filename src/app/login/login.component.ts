9a9576cd405ac907a253cc97cf35e37249938549
gabel10 <zapata.gabriel.abel@gmail.com>
2018-11-07 12:38:31 -0500

    Corrección de Erroes en Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './login.service';
+import { NgFlashMessageService } from 'ng-flash-messages';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService,
+    private flashMessage: NgFlashMessageService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  login(form?: NgForm) {
+    console.log(form.value);
+    //this.loginService.login(form.value).subscribe(res =>{
+    //  var xres = JSON.parse(JSON.stringify(res));
+    //  console.log(xres);
+    //});
   }
 
 }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -5,19 +5,18 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 
-export class Usuario {
-
+/*export class Usuario {
   constructor(usuario = '', password = '') {
       this.usuario = usuario;
       this.password = password;
   }
   usuario : string;
   password : string;
-}
+}*/
 
 export class LoginService {
 
-  private usuario : Usuario;
+  /*private usuario : Usuario;
 
   readonly URL_API = 'http://localhost:3000/api/usuarios';
 
@@ -27,5 +26,5 @@ export class LoginService {
 
   login(user : Usuario){
     return this.http.post(this.URL_API+ 'adminLogin', user);
-  }
+  }*/
 }
