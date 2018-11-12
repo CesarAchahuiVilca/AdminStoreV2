c422bb7c45f0e2c08e22af46b9e66c3e05711e39
paultbr <paultheo65@gmail.com>
2018-11-12 11:04:04 -0500

    m1

--- src/app/marcadistri/marcadistri.component.ts
+++ src/app/marcadistri/marcadistri.component.ts
@@ -2,23 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material';
 
 export interface PeriodicElement {
-  name: string;
+  marca: string;
   position: number;
-  weight: number;
-  symbol: string;
+  codigo: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, codigo:'12', marca: 'Hydrogen'},
+  {position: 2, codigo:'12', marca: 'Helium'},
+  {position: 3, codigo:'12', marca: 'Lithium'},
+  {position: 4, codigo:'12', marca: 'Beryllium'},
+  {position: 5, codigo:'12', marca: 'Boron'},
+  {position: 6, codigo:'12', marca: 'Carbon'},
+  {position: 7, codigo:'12', marca: 'Nitrogen'},
+  {position: 8, codigo:'12', marca: 'Oxygen'},
+  {position: 9, codigo:'12', marca: 'Fluorine'},
+  {position: 10, codigo:'12', marca: 'Neon'},
 ];
 
 @Component({
@@ -28,7 +27,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class MarcadistriComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'codigo', 'marca'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
   value2 = '';
 
