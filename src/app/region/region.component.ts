dcca6dd0a80932b816777c17049c0d0fa2b3624e
gabel10 <zapata.gabriel.abel@gmail.com>
2018-11-20 13:33:23 -0500

    Componente Region versión final

--- src/app/region/region.component.ts
+++ src/app/region/region.component.ts
@@ -18,6 +18,8 @@ export class RegionComponent implements OnInit {
   private provincias : Provincia[];
   private provSelected : Provincia;
   private distritos : string[];
+  private lblDepartamento : string;
+  private lblProvincia : string;
 
   constructor(
     private regionService : RegionService,
@@ -28,13 +30,16 @@ export class RegionComponent implements OnInit {
     this.provSelected = new Provincia('',[]);
     this.depSelected = new Region(null,'',[this.provSelected]);
     this.provincias = [];
+    this.lblDepartamento = 'Nuevo Departamento';
+    this.lblProvincia = 'Nueva Provincia';
     this.getRegiones();
     document.getElementById('divProvincia').hidden = true;
     document.getElementById('divDistrito').hidden = true;
-  }
-
-  agregarRegion(form? : NgForm){
-
+    document.getElementById('btnDepartamento').innerHTML = '<i class="fa fa-plus"></i>';
+    document.getElementById('btnProvincia').innerHTML = '<i class="fa fa-plus"></i>';
+    document.getElementById('spanDepartamento').hidden = true;
+    document.getElementById('spanProvincia').hidden = true;
+    document.getElementById('spanDistrito').hidden = true;
   }
 
   dep_selected(departamento : string){
@@ -44,10 +49,53 @@ export class RegionComponent implements OnInit {
     }
     this.depSelected = this.regionService.regiones[i];
     document.getElementById('divProvincia').hidden = false;
+    this.regionService.departamentoSelected = this.regionService.regiones[i];
+    this.lblDepartamento = 'Editar Departamento'
+    document.getElementById('btnDepartamento').innerHTML = '<i class="fa fa-edit"></i>';
+    document.getElementById('btnDepartamento').style.backgroundColor = 'orange';
+    document.getElementById('spanDepartamento').hidden = false;
   }
 
-  editRegion(region : Region){
+  distSelected(distrito : string){
+    document.getElementById('spanDistrito').hidden = false;
+    console.log(distrito);
+  }
 
+  eliminarDepartamento(){
+    this.regionService.deleteRegion(this.depSelected._id).subscribe(res =>{
+      var jres = JSON.parse(JSON.stringify(res));
+        if(jres.status){
+          this.flashMessage.showFlashMessage({messages: [jres.msg], timeout: 5000, dismissible: true, type: 'success'});
+          this.getRegiones();
+          this.lblDepartamento = 'Nuevo Departamento';
+          document.getElementById('btnDepartamento').innerHTML = '<i class="fa fa-plus"></i>';
+          document.getElementById('btnDepartamento').style.backgroundColor = '#63c2de';
+          document.getElementById('spanDepartamento').hidden = true;
+          this.depSelected = new Region(null,'',[this.provSelected]);
+        }else{
+          this.flashMessage.showFlashMessage({messages: [jres.error], timeout: 5000,dismissible: true, type: 'danger'});
+        }
+    });
+  }
+
+  eliminarDistrito(distrito: string){
+    var i : number = 0;
+    while(this.provSelected.distritos[i] != distrito ){
+      i = i + 1;
+    }
+    this.provSelected.distritos.splice(i,1);
+    this.updateRegion(this.depSelected);
+    this.regionService.distritoSelected = '';
+  }
+
+  eliminarProvincia(provincia : string){
+    var i : number = 0;
+    while(this.depSelected.provincias[i].provincia != provincia){
+      i = i + 1;
+    }
+    this.depSelected.provincias.splice(i,1);
+    this.updateRegion(this.depSelected);
+    this.regionService.provinciaSelected = new Provincia('',[]);
   }
 
   getRegiones() {
@@ -57,13 +105,33 @@ export class RegionComponent implements OnInit {
   }
 
   nuevaProvincia(form?: NgForm){
-    this.depSelected.provincias.push(form.value);
+    if(!form.value._id) {
+      this.depSelected.provincias.push(form.value);
+    } 
     this.updateRegion(this.depSelected);
     form.reset();
+    document.getElementById('btnProvincia').innerHTML = '<i class="fa fa-plus"></i>';
+    document.getElementById('btnProvincia').style.backgroundColor = '#63c2de';
+    document.getElementById('spanProvincia').hidden = true;
+    this.lblProvincia = 'Nueva Provincia';
   }
 
   nuevoDepartamento(form?: NgForm) {
     if(form.value._id) {
+      this.regionService.putRegion(form.value).subscribe(res =>{
+        var jres = JSON.parse(JSON.stringify(res));
+        if(jres.status){
+          this.flashMessage.showFlashMessage({messages: [jres.msg], timeout: 5000, dismissible: true, type: 'success'});
+          this.getRegiones();
+          this.lblDepartamento = 'Nuevo Departamento';
+          document.getElementById('btnDepartamento').innerHTML = '<i class="fa fa-plus"></i>';
+          document.getElementById('btnDepartamento').style.backgroundColor = '#63c2de';
+          document.getElementById('spanDepartamento').hidden = true;
+          form.reset();
+        }else{
+          this.flashMessage.showFlashMessage({messages: [jres.error], timeout: 5000,dismissible: true, type: 'danger'});
+        }
+      })
 
     }else {
       this.regionService.postRegion(form.value).subscribe(res =>{
@@ -92,7 +160,12 @@ export class RegionComponent implements OnInit {
     }
     this.provSelected = this.depSelected.provincias[i] as Provincia;
     this.provSelected.distritos = this.provSelected.distritos ? this.provSelected.distritos : [];
+    this.regionService.provinciaSelected = this.provSelected;
     document.getElementById('divDistrito').hidden = false;
+    document.getElementById('btnProvincia').innerHTML = '<i class="fa fa-edit"></i>';
+    document.getElementById('btnProvincia').style.backgroundColor = 'orange';
+    document.getElementById('spanProvincia').hidden = false;
+    this.lblProvincia = 'Editar Provincia';
   }
 
   resetForm(form?: NgForm) {
