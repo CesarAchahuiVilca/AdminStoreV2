27d430bc3840c814668a276b8c804758686eae78
CesarAchahuiVilca <130341@unsaac.edu.pe>
2018-11-23 13:36:42 -0500

    avance imagenes articulos

--- src/app/articulo/articulo.component.ts
+++ src/app/articulo/articulo.component.ts
@@ -22,13 +22,29 @@ export class ArticuloComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
   dtTriggers: Subject<any> = new Subject();
   flag: boolean = true;
+
   itemsDatosGenerales: [number, string][] = new Array();
   contador_datos_generales = 1;
+
+  itemsCaracteristicas: Number[] = new Array();
+  contador_caracteristicas = 1;
+
+  itemsImagenes: string[] = new Array();
+  contador_imagenes = 1;
+  itemseleccionado: string = "";
+
+
+  readonly URL_API = 'http://localhost:3000/api/imagenes/subir';
+  readonly URL_IMAGES = 'http://localhost:3000/imagenes';
+  selectedFile: File = null;
 
   constructor(private http: HttpClient, private articuloService: ArticuloService) { }
 
   ngOnInit() {
     this.itemsDatosGenerales.push([1,""]);
+    this.itemsCaracteristicas.push(1);
+    this.itemsImagenes.push("imagen-1");
+
     this.getArticulosMysql();
     this.dtOptions = {
       pagingType: 'full_numbers',
@@ -113,6 +129,84 @@ export class ArticuloComponent implements OnInit {
         this.itemsDatosGenerales.splice(i,1);
       }
     }
+  }
+  agregarCaracteristica(){
+    this.contador_caracteristicas = this.contador_caracteristicas+1;
+    this.itemsCaracteristicas.push(this.contador_caracteristicas);
+  }
+  eliminarItemCaracteristica(id:Number){
+    for(var i=0;i<this.itemsCaracteristicas.length;i++){
+      if(this.itemsCaracteristicas[i] == id){
+        this.itemsCaracteristicas.splice(i,1);
+      }
+    }
+  }
+  agregarImagen(){
+    this.contador_imagenes = this.contador_imagenes + 1;
+    this.itemsImagenes.push("imagen-"+this.contador_imagenes);
+  }
+  eliminarItemImagen(id:string){
+    for(var i=0;i<this.itemsImagenes.length;i++){
+      if(this.itemsImagenes[i] == id){
+        this.itemsImagenes.splice(i,1);
+      }
+    }
+  }
+
+  buscarImagen(iditem: string){
+    if(this.itemsImagenes[this.itemsImagenes.length-1]==iditem){
+      document.getElementById("imageninput").click();
+      this.itemseleccionado = iditem;
+    }
+    
+  }
+  subirImagen(evento){
+    this.selectedFile  = <File> evento.target.files[0];
+    //evento.preventDefault();
+    const fd = new FormData();
+    fd.append('image',this.selectedFile, this.selectedFile.name);
+    this.http.post(this.URL_API,fd,{
+      reportProgress: true,
+      observe: 'events'
+    })
+    .subscribe(event=>{
+        if(event.type === HttpEventType.UploadProgress){
+          console.log("Subiendo "+ Math.round(event.loaded/event.total*100)+" %");
+          /*progreso.style.width = Math.round(event.loaded/event.total*100)+"%";
+          progreso.innerHTML = "Subiendo "+ Math.round(event.loaded/event.total*100)+" %";
+          inputfile.style.display="none";*/
+          if(Math.round(event.loaded/event.total*100) == 100){
+            console.log("termino subir la imagen");
+            console.log("Comprimiendo imagen");
+          /*  progreso.innerHTML = "Comprimiendo Imagen....";
+            inputfile.innerHTML = "";*/
+          }
+        
+        }else{
+          if(event.type === HttpEventType.Response){
+            console.log(event.body);
+            var itemImagen = document.getElementById(this.itemseleccionado);
+            var imagenes = itemImagen.getElementsByTagName("img");
+            var simbolo = itemImagen.getElementsByTagName("i")[0];
+            imagenes[0].hidden = false;
+            simbolo.hidden = true;
+            //var  imagen = document.getElementById("imagen-seleccionada") as HTMLImageElement;
+            imagenes[1].hidden= false;
+            itemImagen.style.minWidth = "0px";
+            imagenes[1].src =this.URL_IMAGES+"/tmp/"+this.selectedFile.name;
+           /* progreso.style.backgroundColor = "green";
+            progreso.innerHTML = "Completado.";    */      
+            
+           // this.categoriaService.categoriaSeleccionada.imagen = this.selectedFile.name;
+           //document.getElementById("imagen-seleccionada").hidden=false;
+          // document.getElementById("simbolo-mas").hidden = true;
+          this.agregarImagen();
+          
+
+          }
+        }
+      }
+    );
   }
 
 
