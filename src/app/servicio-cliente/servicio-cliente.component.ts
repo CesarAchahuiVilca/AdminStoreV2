dc13732dee99f70a5063b58a79cf45e1518e687a
CesarAchahuiVilca <130341@unsaac.edu.pe>
2018-12-12 08:54:02 -0500

    avance servicio al cliente

--- src/app/servicio-cliente/servicio-cliente.component.ts
+++ src/app/servicio-cliente/servicio-cliente.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicioClienteService} from './servicio-cliente.service'
+import { MensajeChat} from './mensaje-chat';
+import { Usuario } from '../usuario/usuario';
 
 @Component({
   selector: 'app-servicio-cliente',
@@ -7,15 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicioClienteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private servicioClienteService: ServicioClienteService) { }
+
+  listaMensajesChat: MensajeChat[] = new Array();
 
   ngOnInit() {
+
+    // Cuando alguien i tenta iniciar conversacion
+    this.servicioClienteService.iniciarChat()    
+    .subscribe(res=>{
+      var cliente = res as Usuario;
+      console.log("se conecto "+cliente.nombres);
+      this.servicioClienteService.clienteSeleccionado = res as Usuario;
+      console.log(this.servicioClienteService.clienteSeleccionado);
+    });
+
+    //Al eibir nuevos mensajes
+    this.servicioClienteService.nuevoMensaje()
+    .subscribe(res=>{
+      var mensaje = res as MensajeChat;
+      console.log(mensaje);
+
+    });
   }
   verificarMensaje(event){
     if(event.code=="Enter" && !event.shiftKey){
       var inputmensaje = document.getElementById("contenidomensaje") as HTMLInputElement;
-      inputmensaje.value = "";
+      var mensaje = new MensajeChat("admin",this.servicioClienteService.clienteSeleccionado.correo,inputmensaje.value,"","");
+      this.agregarMensaje(mensaje);
+      this.enviarMensaje(mensaje);
+      
     }
+  }
+  agregarMensaje(mensaje: MensajeChat){
+    this.listaMensajesChat.push(mensaje);
+
+  }
+  enviarMensaje(mensaje: MensajeChat){
+    this.servicioClienteService.enviarMensaje(mensaje);
+
   }
 
 } 
---
+++ src/app/servicio-cliente/servicio-cliente.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
+import { Observable } from 'rxjs';
+import { Usuario } from '../usuario/usuario';
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicioClienteService {
+  private socket  = io('http://localhost:3000');
+
+  idChat:string = "admin"
+
+  clienteSeleccionado: Usuario = new Usuario();
+  usuario: Usuario = new Usuario();
+  constructor() { }
+  enviarMensaje(data){
+    this.socket.emit("chat-admin",data);
+  }
+
+  nuevoMensaje(){
+    let observable = new Observable(observer => {
+      this.socket.on("admin", (data) => {
+        observer.next(data);
+      })
+      return () => {
+        this.socket.disconnect();
+      }
+   });
+   return observable;
+  }
+  iniciarChat(){
+    let observable = new Observable(observer => {
+      this.socket.on("init-admin", (data) => {
+        observer.next(data);
+      });
+      return () => {
+        this.socket.disconnect();
+      }
+    });
+    return observable;
+  }
+}
